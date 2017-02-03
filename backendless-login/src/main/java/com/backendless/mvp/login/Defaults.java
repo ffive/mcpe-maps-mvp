39689058c0c0ddc7edaca67bca5bf58f4efd6fc1
yurikomlev <yurasopakomlev@gmail.com>
2017-02-03 17:26:05 +0300

    sync with staging, added description

---
+++ backendless-login/src/main/java/com/backendless/mvp/login/Defaults.java
@@ -0,0 +1,9 @@
+package com.backendless.mvp.login;
+
+public class Defaults
+{
+  public static final String APPLICATION_ID = "D6C4C848-6DBF-3FDA-FF9B-5EAC025EB500";
+  public static final String SECRET_KEY = "B69F271E-04E3-B179-FF94-7D8C68295500";
+  public static final String VERSION = "v1";
+  public static final String SERVER_URL = "https://api.backendless.com";
+}
