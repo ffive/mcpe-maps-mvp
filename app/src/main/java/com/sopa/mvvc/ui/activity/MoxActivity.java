0ab792e864d19cb5343df4a11531207daeb95767
yurikomlev <yurasopakomlev@gmail.com>
2017-02-07 02:07:43 +0300

    - added NavigationDrawer code to MoxActivity - header jpg went to res/mipmap - added layout( only imgview for now)  for drawer

--- app/src/main/java/com/sopa/mvvc/ui/activity/MoxActivity.java
+++ app/src/main/java/com/sopa/mvvc/ui/activity/MoxActivity.java
@@ -7,18 +7,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.util.ListUpdateCallback;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.PresenterType;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.sopa.mvvc.R;
 import com.sopa.mvvc.databinding.ActivityMoxBinding;
 import com.sopa.mvvc.datamodel.local.MyDiffCallback;
@@ -69,6 +73,26 @@ public class MoxActivity extends MvpAppCompatActivity implements MoxView, UserCo
     private static Fragment getUploadMapFragment ( ) {
         Log.d (TAG, "getUploadMapFragment: created UploadMapFragment ");
         return new UploadMapFragment ( );
+    }
+
+    public void initNavigationDrawer(){
+
+        new Drawer ()
+                .withActivity(this)
+                .withToolbar(binding.toolbar)
+                .withActionBarDrawerToggle(true)
+                .withHeader(R.layout.drawer_header)
+                .addDrawerItems(
+                        new PrimaryDrawerItem ().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
+                        new SectionDrawerItem ().withName(R.string.drawer_item_settings),
+                        new SecondaryDrawerItem ().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_cog),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
+                        new DividerDrawerItem (),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_github).withBadge("12+").withIdentifier(1)
+                )
+                .build();
     }
 
 
