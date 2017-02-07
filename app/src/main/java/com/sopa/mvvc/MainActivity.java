ff6fe542326cd87428d0db426f9a5fd5b5b65e92
yurikomlev <yurasopakomlev@gmail.com>
2017-02-07 18:22:04 +0300

    - submodule moved

--- app/src/main/java/com/sopa/mvvc/MainActivity.java
+++ app/src/main/java/com/sopa/mvvc/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.backendless.BackendlessUser;
 import com.backendless.mvp.login.Defaults;
 import com.sopa.mvvc.ui.activity.MoxActivity;
 
@@ -17,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
 
 
-        startActivityForResult (new Intent (MainActivity.this, com.backendless.mvp.login.LoginActivity.class),420);// Defaults.REQUEST_LOGIN_CODE);
+        startActivityForResult (new Intent (MainActivity.this, com.backendless.mvp.login.LoginActivity.class),Defaults.REQUEST_CODE);// Defaults
+        // .REQUEST_LOGIN_CODE);
 
 
     }
@@ -25,28 +25,32 @@ public class MainActivity extends AppCompatActivity {
     //Will be used for some other modules/requests too, trying to generify:
     @Override
     protected void onActivityResult ( int requestCode, int resultCode, Intent data ) {
+        super.onActivityResult (requestCode, resultCode, data);
 
-
-       // if ( //requestCode == Defaults.REQUEST_LOGIN_CODE ) {
-            BackendlessUser backendlessUser;
+        if ( requestCode == Defaults.REQUEST_CODE ) {
+          //  BackendlessUser backendlessUser;
             switch ( resultCode ) {
 
+
                 case Defaults.VALID_LOGIN: {
+
+                    //backendlessUser = ( BackendlessUser ) data.getSerializableExtra ("backendless_user");
                     startActivity (new Intent (MainActivity.this, MoxActivity.class));
-                    backendlessUser = ( BackendlessUser ) data.getSerializableExtra ("backendless_user");
                     break;
                 }
                 case Defaults.LOGIN_SUCCESS: {
+
+                  // backendlessUser = ( BackendlessUser ) data.getSerializableExtra ("backendless_user");
                     startActivity (new Intent (MainActivity.this, MoxActivity.class));
-                    backendlessUser = ( BackendlessUser ) data.getSerializableExtra ("backendless_user");
                     break;
                 }
-                default:break;
+                default:{startActivity (new Intent (MainActivity.this, MoxActivity.class));}
             }
 
 
-            //
-            //  super.onActivityResult (requestCode, resultCode, data);
+
+              //
       //  }
     }
 }
+    }
